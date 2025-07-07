28292137d5719a8815edc77696e3c775ea200c8c
RabbanMehamood <rabbansk1729@gmail.com>
2025-07-08 02:31:16 +0530

    rendering icons success

--- src/app/organisation-chart/organisation-chart.component.ts
+++ src/app/organisation-chart/organisation-chart.component.ts
@@ -7,42 +7,31 @@ import {
 import OrgChart from 'orgchart.js/src/orgchart.js';
 
 let datasource = {
-  id: '1',
   name: 'Lao Lao',
   title: 'general manager',
   children: [
-    { id: '2', name: 'Bo Miao', title: 'department manager' },
+    { name: 'Bo Miao', title: 'department manager' },
     {
-      id: '3',
       name: 'Su Miao',
       title: 'department manager',
       children: [
-        { id: '4', name: 'Tie Hua', title: 'senior engineer' },
+        { name: 'Tie Hua', title: 'senior engineer' },
         {
-          id: '5',
           name: 'Hei Hei',
           title: 'senior engineer',
           children: [
-            { id: '6', name: 'Dan Zai', title: 'engineer' },
-            { id: '7', name: 'Dan Dan', title: 'engineer' },
-            { id: '8', name: 'Xiang Xiang', title: 'engineer' },
-            { id: '9', name: 'Ke Xin', title: 'engineer' },
-            { id: '10', name: 'Xiao Dan', title: 'engineer' },
-            { id: '11', name: 'Dan Dan Zai', title: 'engineer' },
+            { name: 'Pang Pang', title: 'engineer' },
+            { name: 'Xiang Xiang', title: 'UE engineer' },
           ],
         },
-        { id: '12', name: 'Pang Pang', title: 'senior engineer' },
-        { id: '13', name: 'Er Pang', title: 'senior engineer' },
-        { id: '14', name: 'San Pang', title: 'senior engineer' },
-        { id: '15', name: 'Si Pang', title: 'senior engineer' },
       ],
     },
-    { id: '16', name: 'Hong Miao', title: 'department manager' },
-    { id: '17', name: 'Chun Miao', title: 'department manager' },
-    { id: '18', name: 'Yu Li', title: 'department manager' },
-    { id: '19', name: 'Yu Jie', title: 'department manager' },
-    { id: '20', name: 'Yu Wei', title: 'department manager' },
-    { id: '21', name: 'Yu Tie', title: 'department manager' },
+    { name: 'Yu Jie', title: 'department manager' },
+    { name: 'Yu Li', title: 'department manager' },
+    { name: 'Hong Miao', title: 'department manager' },
+    { name: 'Yu Wei', title: 'department manager' },
+    { name: 'Chun Miao', title: 'department manager' },
+    { name: 'Yu Tie', title: 'department manager' },
   ],
 };
 
@@ -108,6 +97,7 @@ export class OrganisationChartComponent {
       pan: true,
       zoom: true,
       draggable: true,
+      parentNodeSymbol: 'fa-users',
     });
   }
 }
